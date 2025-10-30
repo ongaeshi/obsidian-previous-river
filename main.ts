3a5b7eb70a0daf25c4acc7f7b289bc4eb87c4e8d
ongaeshi <ongaeshi0621@gmail.com>
2025-10-30 22:18:10 +0900

    feat: Add go-to-previous-note and go-to-next-note commands

--- main.ts
+++ main.ts
@@ -1,134 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, Notice } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class PreviousRiverPlugin extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: "go-to-previous-note",
+      name: "前のノートに移動",
+      callback: () => this.goToPreviousNote(),
+    });
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    this.addCommand({
+      id: "go-to-next-note",
+      name: "次のノートに移動",
+      callback: () => this.goToNextNote(),
+    });
+  }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  getActiveFile(): TFile | null {
+    return this.app.workspace.getActiveFile();
+  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async goToPreviousNote() {
+    const file = this.getActiveFile();
+    if (!file) {
+      new Notice("アクティブなノートがありません");
+      return;
+    }
 
-	async onload() {
-		await this.loadSettings();
+    const content = await this.app.vault.read(file);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // YAML frontmatter または行頭に "previous: [[ノート名]]" がある場合を探す
+    const match = content.match(/^previous:\s*\[\[(.+?)\]\]/m);
+    if (!match) {
+      new Notice("previous プロパティが見つかりません");
+      return;
+    }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    const previousNoteName = match[1];
+    const target = this.app.metadataCache.getFirstLinkpathDest(previousNoteName, file.path);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    if (!target) {
+      new Notice(`ノート「${previousNoteName}」が見つかりません`);
+      return;
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    await this.app.workspace.getLeaf().openFile(target);
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async goToNextNote() {
+    const file = this.getActiveFile();
+    if (!file) {
+      new Notice("アクティブなノートがありません");
+      return;
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    // 🔍 バックリンク情報を取得
+    const backlinks = this.app.metadataCache.resolvedLinks;
+    const currentPath = file.path;
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    const nextNotes: TFile[] = [];
 
-	onunload() {
+    // resolvedLinks は { "noteA.md": { "noteB.md": count, ... } } のような構造
+    for (const [sourcePath, links] of Object.entries(backlinks)) {
+      if (links[currentPath]) {
+        const targetFile = this.app.vault.getAbstractFileByPath(sourcePath);
+        if (targetFile instanceof TFile) {
+          nextNotes.push(targetFile);
+        }
+      }
+    }
 
-	}
+    if (nextNotes.length === 0) {
+      new Notice("次のノート（バックリンク）が見つかりません");
+      return;
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    if (nextNotes.length === 1) {
+      await this.app.workspace.getLeaf().openFile(nextNotes[0]);
+    } else {
+      const list = nextNotes.map(f => f.basename).join("\n");
+      new Notice(`複数の次ノートがあります:\n${list}`);
+    }
+  }
 }
