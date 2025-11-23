210cc72500a74911e5ff635d4420322d3fa96373
ongaeshi <ongaeshi0621@gmail.com>
2025-11-23 15:56:42 +0900

    feat: Add generate_notes.rb script

---
+++ tests/vault/previous-river-test/04_many_previous/generate_notes.rb
@@ -0,0 +1,39 @@
+# note_generator.rb
+# Usage: ruby note_generator.rb <number_of_notes>
+
+require 'fileutils'
+
+def generate_notes(directory, name, count)
+    FileUtils.mkdir_p(directory) unless Dir.exist?(directory)
+
+    count.times do |i|
+        previous_value = if i == 0
+          "ROOT"
+        else 
+          "[[#{name}_#{format('%02d', (i - 1) % count)}]]"
+        end
+        current_note = "#{name}_#{format('%02d', i)}.md"
+
+        content = <<~MARKDOWN
+            ---
+            previous: "#{previous_value}"
+            ---
+        MARKDOWN
+
+        File.write(File.join(directory, current_note), content)
+    end
+end
+
+if ARGV.size != 1
+    puts "Usage: ruby #{$PROGRAM_NAME} <number_of_notes>"
+    exit 1
+end
+
+note_count = ARGV[0].to_i
+if note_count <= 0
+    puts "Please provide a positive number of notes."
+    exit 1
+end
+
+generate_notes('.', "note", note_count)
+puts "Generated #{note_count} notes."
